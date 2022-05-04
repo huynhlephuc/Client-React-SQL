ef6d254563f4348a9ec666790cf06336938ac816
unknown <you@example.com>
2022-05-05 00:22:42 +0700

    backup

--- components/Qr/index.jsx
+++ components/Qr/index.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, Image } from 'react-native';
+import { Text, View, StyleSheet, Button, Image,ScrollView } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import axios from 'axios';
+import moment from 'moment'
 
 export default function Qr() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [id, setId] = useState(null);
-  const [data, setData] = useState([]);
-  
+  const [loading, setLoading] = useState(true);
+  const [loHaiSan, setLoHaiSan] = useState({});
+  const [sanPham, setSanPham] = useState({});
+  const [donViNuoi, setDonViNuoi] = useState({});
    
 
   useEffect(() => {
@@ -25,9 +28,13 @@ export default function Qr() {
     setScanned(true);
     /* alert(`Bar code with type ${type} and data ${data} has been scanned!`); */
    console.log(data);
-   const gethttp = await axios.get(`http://192.168.1.15:3001/data/${data}`);
-   console.log(gethttp.data);
-   setData(gethttp.data);
+   const getLoHaiSan = await axios.get(`http://192.168.1.6:3001/data/${data}`);
+   setLoHaiSan(getLoHaiSan.data.data[1]);
+   const getsanpham = await axios.get(`http://192.168.1.6:3001/data/sanpham/${getLoHaiSan.data.data[1].idsanpham}`);
+   setSanPham(getsanpham.data[0]);
+   const getDonViNuoi = await axios.get(`http://192.168.1.6:3001/data/donvinuoi/${getLoHaiSan.data.data[1].idnuoi}`);
+   setDonViNuoi(getDonViNuoi.data[0]);
+   setLoading(false)
   };
 
   if (hasPermission === null) {
@@ -39,6 +46,7 @@ export default function Qr() {
 
   return (
     <>
+    <ScrollView>
     <View style={styles.barcodebox}>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
@@ -47,17 +55,29 @@ export default function Qr() {
       {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View >
     <View style={styles.qrdata}>
-      {data && data.map((item, index) => {
-        return (
-          <View key={index}>
-            <Text>Tên sản phẩm: {item.tensanpham}</Text>
-            <Image source={{uri: `${item.anh}`}} style={{width: 300, height: 140}} />
-            <Text>Giá của sản phẩm{item.giasanpham}</Text>
-            <Text>Mô tả{item.mota}</Text>
+         {!loading ?  
+         <View style={styles.textcs}>
+           <Text  style={{fontSize: 20, fontWeight: 'bold'}}>SAN PHAM </Text>
+            <Text>Tên sản phẩm: {sanPham.tensanpham}</Text>
+            <Image source={{uri: `${sanPham.anh}`}} style={{width: 300, height: 140}} />
+            <Text>Giá của sản phẩm{sanPham.giasanpham}</Text>
+            <Text>Mô tả{sanPham.mota}</Text>
+           <Text style={{fontSize: 20,fontWeight: 'bold'}}>LO HAI SAN </Text>
+           <Text>Khoi luong: {loHaiSan.khoiluong}</Text>
+           <Text>Ngay danh bat: {moment(loHaiSan.ngaydanhbat).format('DD/MM/YYYY')}</Text>
+           <Text>Ngay che bien: {moment(loHaiSan.ngaychebien).format('DD/MM/YYYY')}</Text>
+           <Text>Ngay nuoi: {moment(loHaiSan.ngaynuoi).format('DD/MM/YYYY')}</Text>
+           <Text style={{fontSize: 20,fontWeight: 'bold'}}>DON VI NUOI </Text>
+           <Text>Giá của sản phẩm{donViNuoi.diachinuoi}</Text>
+           <Text>Giá của sản phẩm{donViNuoi.sdt_dvnuoi}</Text>
+           <Text>So luong sản phẩm{loHaiSan.soluong}</Text>
+
           </View>
-        )
-      })}
+           : <Text>loading...</Text>
+        }
     </View>
+    </ScrollView>
+
     </>
     
 
@@ -78,6 +98,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     overflow: 'hidden',
     borderRadius: 30,
+ },
+ textcs: {
+  alignItems: 'center',
+  justifyContent: 'center',
+  overflow: 'hidden',
+  borderRadius: 30,
  }, 
 }); 
 
